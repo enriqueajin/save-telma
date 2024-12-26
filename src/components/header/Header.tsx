1f52307e075024c32d2607bac1d480090d80d6b7
Enrique Ajin <enriquesaon@gmail.com>
2024-12-26 20:26:14 +0100

    install tailwind. Add tailwind scripts to package.json. Add styles.css to .gitignore. Define theme colors within tailwind.config. Create components package and empty Header component.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header () {
+  return (
+    <header className='h-24 bg-pastelPink'>
+
+    </header>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pastelPink: "#FFB9B9",
+        softPink: "#FFE3E3",
+        vividPink: "#FF0059",
+        softPurple: "#C9CDF9",
+        darkPurple: "#4920AF",
+        softBlue: "#DAE4F8",
+        darkBlue: "#0F43AC",
+        beige: "#F3E4D7",
+        darkBeige: "#E0CCBC",
+        chocolateBrown: "#4A0707",
+        darkBrown: "#504032"
+      }
+    },
+  },
+  plugins: [],
+}
+
